fbdeb74cec2a074dc4612d337fc5d656b603e3f4
Chris <asonetu@naver.com>
2018-01-26 10:54:10 +0900

    gitignore 추가, LoginViewController 추가

---
+++ between/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  between
+//
+//  Created by 김홍식 on 2018. 1. 26..
+//  Copyright © 2018년 김홍식. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
