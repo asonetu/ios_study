81c062c9753d02278e973ea6bc2d666165af64b4
ghdhddnr92 <ghdehddnr92@gmail.com>
2018-02-21 17:13:14 +0900

    회원가입 화면 UI 수정

--- between/auth/viewController/SignUpViewController.swift
+++ between/auth/viewController/SignUpViewController.swift
@@ -18,9 +18,18 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var userPasswordTextField: UITextField!
     @IBOutlet weak var coupleEmailTextField: UITextField!
     
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var userEmailLabel: UILabel!
+    @IBOutlet weak var userPasswrodLabel: UILabel!
+    @IBOutlet weak var coupleEmailLabel: UILabel!
+
+    @IBOutlet weak var signUpButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        signUpButton.layer.cornerRadius = 10
         // Do any additional setup after loading the view.
     }
 
